5d50c033e36ba67340563191b2d07afcde27591e
Lam Bui <lamb@marvell.com>
2016-01-20 07:24:03 -0800

    Commented template to do the starfield.

---
+++ starfield_blind2/arm9/source/template.c
@@ -0,0 +1,25 @@
+/*** 
+
+	Starfield demo
+	- Simulate a starfield using a framebuffer (no background tiles or sprites)
+	- We will do the logic and drawing during the Vblank period
+
+***/
+
+#include <nds.h>
+
+int main() {
+
+	// enable the Vblank interrupt
+	// setup our video mode
+	// enable our Vram (only what we need)
+
+	// initialize our starfield
+
+	// do logic and drawing for our starfield during Vblank
+	while(1) {
+
+	}
+
+	return 0;
+}
