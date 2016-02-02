e14371a822f9de27f5f73e8f6765908fe84736ae
Lam Bui <lamb@marvell.com>
2016-02-01 18:10:38 -0800

    First post of sprite demo with F-18

---
+++ sprite-F-18/source/main.c
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------------------------------
+
+	simple sprite demo using an F-18 to move around
+
+---------------------------------------------------------------------------------*/
+#include <nds.h>
+
+int main() {
+	// setup our video mode
+	// select what VRAM banks we want to use for sprite data
+	// initialize our OAM
+	// reserve our OAM memory for the sprites
+	// fill our reserved OAM memory with the actual sprite maps
+	// load our palette data
+
+	while(1) {
+		// setup our sprite affine matrix
+		// configure our sprite OAM data
+
+		// update the OAM with our changes
+	}
+
+	return 0;
+}
