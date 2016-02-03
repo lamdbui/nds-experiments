d6b1d043bdea7650cfe9001b35d6a165d324920c
Lam Bui <lamb@marvell.com>
2016-02-02 17:44:09 -0800

    Add rotation when pressing the L/R buttons

--- sprite-F-18/source/main.c
+++ sprite-F-18/source/main.c
@@ -8,15 +8,21 @@
 	[TODO] Figure out what to do to add a thurst animation
 	[TODO] Try adding a space/sky background
 	[TODO] Make space/sky background move appropriately in relation to direction of the ship
+	[TODO] Use the sprite sheet instead
+	[TODO] Switch to different sprite with yaw and bank (D-pad)
 
 ---------------------------------------------------------------------------------*/
 #include <nds.h>
 
 #include "F-18-2-0.h"
 
+#define ROTATION_RATE 2
+
 int main() {
 
 	u16 *mainSpriteMem;
+	int shipAngle = 0;
+	int keys;
 
 	// setup our interrupts to draw at the appropriate time
 	irqInit();
@@ -43,11 +49,34 @@ int main() {
 	dmaCopy(F_18_2_0Pal, SPRITE_PALETTE, F_18_2_0PalLen);
 
 	while(1) {
+
+		// scan for input
+		scanKeys();
+		keys = keysHeld();
+
+		// handle our input
+		if(keys & KEY_L) {
+			shipAngle += ROTATION_RATE;
+			if(shipAngle > 360) {
+				shipAngle -= 360;
+			}
+		}
+		if(keys & KEY_R) {
+			shipAngle -= ROTATION_RATE;
+			if(shipAngle < 0) {
+				shipAngle = 360 - shipAngle;
+			}
+		}
+
 		// setup our sprite affine matrix
-		//void oamRotateScale(OamState* oam, int rotId, int angle, int sx, int sy);
-		//oamRotateScale()
+		oamRotateScale(&oamMain,		// main graphics engine context
+			0,							// set rotation matrix index (we can have up to 32)			
+			degreesToAngle(shipAngle),	// convert to binary radians (-32768 to 32767) for nds
+			(1 << 8),					// 8.8 fixed point inverse scale factor X
+			(1 << 8)					// 8.8 fixed point inverse scale factor Y
+			);
+
 		// configure our sprite OAM data
-		
 		oamSet(&oamMain,				// main graphics engine context
 			0,           				// oam index (0 to 127)  
 			100, 50,   					// x and y pixel location of the sprite
@@ -56,8 +85,8 @@ int main() {
 			SpriteSize_32x32,     
 			SpriteColorFormat_16Color, 
 			mainSpriteMem,              //pointer to the loaded graphics
-			-1,                  		//sprite rotation/scale matrix index 
-			false,               		//double the size when rotating?
+			0,                  		//sprite rotation/scale matrix index 
+			true,               		//double the size when rotating?
 			false,						//hide the sprite?
 			false, false, 				//vflip, hflip
 			false						//apply mosaic
